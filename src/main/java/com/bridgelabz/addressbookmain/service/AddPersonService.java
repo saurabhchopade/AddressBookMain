08810bbe7f94982305ec215cf4c83c7105f3bac5
saurabh chopade <saurabhchopade3250@gmail.com>
2020-08-06 10:14:50 +0530

    ADDED : Code make connection with database

--- src/main/java/com/bridgelabz/addressbookmain/service/AddPersonService.java
+++ src/main/java/com/bridgelabz/addressbookmain/service/AddPersonService.java
@@ -2,6 +2,6 @@ package com.bridgelabz.addressbookmain.service;
 import org.json.simple.parser.ParseException;
 
 import java.io.IOException;
-interface  AddPersonService {
+interface  AddPersonService{
     void addPerson(int personCounter) throws IOException, ParseException;
 }
--- src/test/java/RESTAssureTest.java
+++ src/test/java/RESTAssureTest.java
@@ -1,1 +1,27 @@
-public class RESTAssureTest {}
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.hamcrest.Matchers;
+import org.junit.Before;
+import org.junit.Test;
+public class RESTAssureTest {
+
+    @Before
+    public void setUp()  {
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = 3000;
+    }
+    @Test
+    public Response getPersonList() {
+    Response response=RestAssured.get("/home/saurabh/IdeaProjects/AaddressBookMain/src/main/resources/");
+    return response;
+    }
+
+    @Test
+    public void onCallingList_ReturnPersonList() {
+        Response response=getPersonList();
+        System.out.println("First: " + response.asString());
+        response.then().body("id", Matchers.hasItems(1,2));
+        response.then().body("title", Matchers.hasItems("json-server"));
+
+    }
+}
